56f03238076a321961ec8ae293be3981d296cdeb
Ndiaga60 <ndiaga.ndiaye@outlook.com>
2023-11-14 00:28:51 +0100

    amélioration numéro 1: fonction

--- code.py
+++ code.py
@@ -1,9 +1,12 @@
-montant_note = float(input('Entrez le montant de la note : '))
-pourcentage_pourboire=float(input("quel est le pourcentage du pourboire que vous voulez donner comparé a la note?"))
-calcul_pourboire=(montant_note*pourcentage_pourboire)/100)
-r=montant_note+calcul_pourboire
+def pourboire(montant_note,pourcentage_note):
 
-print(f"Montant de la note : {montant_note}$")
-print(f"Pourboire({pourcentage_note}$): {calcul_pourboire}$")
-print(f"Montant total a payer : {r}$")
+	calcul_pourboire=(montant_note*pourcentage_pourboire)/100)
+	r=montant_note+calcul_pourboire
+	
+	print(f"Montant de la note : {montant_note}$")
+	print(f"Pourboire({pourcentage_note}$): {calcul_pourboire}$")
+	print(f"Montant total a payer : {r}$")
 
+x=float(input("quel est la note a payer? : "))
+y=float(input("quel est le pourcentage de la note que vous vouliez attribuer comme pourboire?"))
+pourboire(x,y)
