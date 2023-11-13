2baff19365a03a0556b64f2f0919042c35b36ab2
Ndiaga60 <ndiaga.ndiaye@outlook.com>
2023-11-14 00:52:36 +0100

    amélioration numéro 2: diviser la note

--- code.py
+++ code.py
@@ -2,11 +2,24 @@ def pourboire(montant_note,pourcentage_note):
 
 	calcul_pourboire=(montant_note*pourcentage_pourboire)/100)
 	r=montant_note+calcul_pourboire
+	diviser=input("voulez vous diviser le pourboire?")
+
 	
-	print(f"Montant de la note : {montant_note}$")
-	print(f"Pourboire({pourcentage_note}$): {calcul_pourboire}$")
-	print(f"Montant total a payer : {r}$")
+	if diviser=="oui":
+		nombre=int(input("par combien?")
+		calcul_pourboire=calcul_pourboire/nombre
+		print(f"Montant de la note : {montant_note}$")
+		print(f"Pourboire({pourcentage_note}$): {calcul_pourboire}$")
+		return
+	else:
+		print(f"Montant de la note : {montant_note}$")
+                print(f"Pourboire({pourcentage_note}$): {calcul_pourboire}$")
+                print(f"Montant total a payer : {r}$")
+ 
+		return
 
 x=float(input("quel est la note a payer? : "))
 y=float(input("quel est le pourcentage de la note que vous vouliez attribuer comme pourboire?"))
 pourboire(x,y)
+
+♠
