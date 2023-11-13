c147786f1c5102e3094fa990b4d2631a957e6e8a
Ndiaga60 <ndiaga.ndiaye@outlook.com>
2023-11-14 00:11:38 +0100

    affichage des résultats

--- code.py
+++ code.py
@@ -1,7 +1,9 @@
 montant_note = float(input('Entrez le montant de la note : '))
 pourcentage_pourboire=float(input("quel est le pourcentage du pourboire que vous voulez donner comparé a la note?"))
 calcul_pourboire=(montant_note*pourcentage_pourboire)/100)
-print(calcul_pourboire)
+r=montant_note+calcul_pourboire
 
-
+print(f"Montant de la note : {montant_note}$")
+print(f"Pourboire({pourcentage_note}$): {calcul_pourboire}$")
+print(f"Montant total a payer : {r}$")
 
