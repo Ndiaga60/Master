d35da35ef0171edd2e89010e5081c0f7fe77dd77
Ndiaga60 <ndiaga.ndiaye@outlook.com>
2023-11-13 19:45:07 +0100

    ajout de la ligne du pourcentage

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
-montant_note=float(input("Entrez le montant de la note : "))
+montant_note = float(input('Entrez le montant de la note : '))
+pourcentage_pourboire=float(input("quel est le pourcentage du pourboire que vous voulez donner comparé a la note?"))
+
 
 
